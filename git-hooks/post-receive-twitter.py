605a3de03cbf753507483f0f64d257067f6a002f
Adam Lloyd <lloyda2@rpi.edu>
2009-06-28 06:05:01 -0400

    Add copyright notices to files.

--- git-hooks/post-receive-twitter.py
+++ git-hooks/post-receive-twitter.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-# script to update cirquetidev on twitter with git commits
+# script to update Twitter with git commits
+#
+# Copyright (C) 2009 Adam Lloyd <lloyda2 (at) rpi (dot) edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 
