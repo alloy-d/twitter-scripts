99a62cb0998897f094b0e86af07d52dd7f778d05
Adam Lloyd <lloyda2@rpi.edu>
2009-07-02 08:28:46 -0400

    Don't use string.format() in order to make old Python happy. *grumble grumble*

--- hg-hooks/hg_twitter_hooks.py
+++ hg-hooks/hg_twitter_hooks.py
@@ -16,10 +16,8 @@ def incoming(ui, repo, node, **kwargs):
 
     changed_files = len(changeset.files())
 
-    update_text = "{0} [{1} changed]".format(
-        description_summary,
-        changed_files,
-        )
+    update_text = description_summary
+    update_text += ' [' + str(changed_files) + ' changed]'
 
     twitter_api.PostUpdate(update_text)
     return False
